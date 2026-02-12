c54d9423ce784d595950dd0f9ac1fe08d2b5bb77
Mala4505 <aliasgerzmala@gmail.com>
2026-02-12 03:07:09 +0300

    notifications

--- users/views.py
+++ users/views.py
@@ -1,29 +1,3 @@
-# from rest_framework.decorators import api_view
-# from rest_framework.response import Response
-# from django.views.decorators.csrf import csrf_exempt
-# from rest_framework import status
-# import os
-
-# @csrf_exempt
-# # @require_POST
-# @api_view(["POST"])
-# def admin_login(request):
-#     """Verify admin password. Frontend sends X-Admin-Password or JSON body."""
-#     password = (
-#         request.headers.get("X-Admin-Password")
-#         or request.data.get("password")
-#         or request.POST.get("password")
-#     )
-#     expected = os.environ.get("ADMIN_PASSWORD")
-    
-#     print("Submitted password:", password)
-#     print("Expected ADMIN_PASSWORD:", expected)
-    
-#     if not expected or password != expected:
-#         return Response({"detail": "Invalid password."}, status=status.HTTP_401_UNAUTHORIZED)
-#     return Response({"success": True})
-
-
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.views.decorators.csrf import csrf_exempt
